dc894d4cb6c2b23bb400bacba8a17b521925aa2d
kyriosaa <kyriosaaph@gmail.com>
2025-03-11 15:59:17 +0700

    03-arithmetic

---
+++ classes/brocode-javascript/03-arithmetic/index.js
@@ -0,0 +1,48 @@
+let students = 30;
+
+// + plus
+students += 1;
+// students = students + 1;
+console.log(students);
+
+// - minus
+students -= 1;
+// students = students - 1;
+console.log(students);
+
+// * multiply
+students *= 2;
+// students = students * 2;
+console.log(students);
+
+// / divide
+students /= 2;
+// students = students / 2;
+console.log(students);
+
+// ** exponent
+students **= 2;
+// students = students ** 2
+console.log(students);
+
+// % modulus
+students %= 2;
+// students = students % 2
+console.log(students);
+
+// ++ increment
+students++;
+console.log(students);
+
+// -- decrement
+students--;
+console.log(students);
+
+
+// --- operator precedence --- 
+//     PEMDAS
+//     Parentheses()
+//     Exponents
+//     Multiplication, Division, Modulus
+//     Addition, Subtraction
+// ---------------------------
