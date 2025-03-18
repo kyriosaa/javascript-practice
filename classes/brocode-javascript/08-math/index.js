1675f3b5b0ca62262f7e39865cdb3569426ec4bc
kyriosaa <kyriosaaph@gmail.com>
2025-03-18 18:01:06 +0700

    08-math

---
+++ classes/brocode-javascript/08-math/index.js
@@ -0,0 +1,31 @@
+// Math = built-in object that provides a collection of properties and methods
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);      // rounds number
+// z = Math.floor(x);      // rounds down
+// z = Math.ceil(x);       // rounds up
+// z = Math.trunc(x);      // removes decimals
+// z = Math.pow(x, y);     // power
+// z = Math.sqrt(x);       // square root
+// z = Math.log(x);        // logarithm
+// z = Math.sin(x);        // sine
+// z = Math.cos(x);        // cosine
+// z = Math.tan(x);        // tangent
+// z = Math.abs(x);        // absolute value
+// z = Math.sign(x);       // gives the sign (1, -1)
+
+// console.log(z);
+
+// --------------------------------------------------------
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(max);
+console.log(min);
